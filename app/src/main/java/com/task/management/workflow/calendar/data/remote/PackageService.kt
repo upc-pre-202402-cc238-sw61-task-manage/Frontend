5d4630573011c022bb43945f2853246d06463a12
PieroM1 <105945018+PieroM1@users.noreply.github.com>
2024-09-25 18:31:13 -0500

    feat(calendar): added PackageRepository.kt

--- app/src/main/java/com/task/management/workflow/calendar/data/remote/PackageService.kt
+++ app/src/main/java/com/task/management/workflow/calendar/data/remote/PackageService.kt
@@ -3,7 +3,6 @@ package com.task.management.workflow.calendar.data.remote
 
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Headers
 import retrofit2.http.Path
 
 
---
+++ app/src/main/java/com/task/management/workflow/calendar/data/repository/PackageRepository.kt
@@ -0,0 +1,30 @@
+package com.task.management.workflow.calendar.data.repository
+
+
+import com.task.management.workflow.calendar.data.remote.PackageDto
+import com.task.management.workflow.calendar.data.remote.PackageService
+import com.task.management.workflow.calendar.data.remote.toPackage
+import com.task.management.workflow.calendar.domain.EventPackage
+import com.task.management.workflow.common.Resource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+
+class PackageRepository(private val service: PackageService) {
+    suspend fun getPackages(userId: Int): Resource<List<EventPackage>> = withContext(Dispatchers.IO){
+        try {
+            val response = service.getEventsbyUser(userId)
+            if(response.isSuccessful){
+                response.body()?.let { packageDtos: List<PackageDto> ->
+                    val packages = packageDtos.map { packageDto: PackageDto ->
+                        packageDto.toPackage()
+                    }.toList()
+                    return@withContext Resource.Success(data = packages)
+                }
+            }
+            return@withContext Resource.Error(message = response.message())
+        } catch (e: Exception){
+            return@withContext Resource.Error(message = e.message?: "An exception occurred")
+        }
+    }
+}
