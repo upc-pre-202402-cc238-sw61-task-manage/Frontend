270ecd27350cf53ebe0f240e50d43bb83073f7de
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2024-09-25 14:32:09 -0500

    feat: added the common files
    this includes the Constants, Resource and UIState.

---
+++ app/src/main/java/com/task/management/workflow/common/Constants.kt
@@ -0,0 +1,5 @@
+package com.task.management.workflow.common
+
+object Constants {
+    const val BASE_URL = "http://localhost:8080"
+}
---
+++ app/src/main/java/com/task/management/workflow/common/UIState.kt
@@ -0,0 +1,7 @@
+package com.task.management.workflow.common
+
+data class UIState<T>(
+    val isLoading: Boolean = false,
+    var data: T? = null,
+    val message: String = ""
+)
