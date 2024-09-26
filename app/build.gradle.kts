2273de8b844fdfe59a0b6451cb467d4bae593848
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2024-09-26 14:10:14 -0500

    Revert "feat: added the navigation-compose"
    This reverts commit 7411b2efacee81a401762463f76bdbcc6b192ac5.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,8 +60,6 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.androidx.material.navigation)
-    implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
