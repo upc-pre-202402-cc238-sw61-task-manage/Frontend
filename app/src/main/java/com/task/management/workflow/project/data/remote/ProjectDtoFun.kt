25ea7256c0a6f07ded07513a04ecf187412c1142
rrenzosilva <149326018+rrenzosilva@users.noreply.github.com>
2024-10-23 23:16:06 -0500

    fix: project dto fun

--- app/src/main/java/com/task/management/workflow/project/data/remote/ProjectDtoFun.kt
+++ app/src/main/java/com/task/management/workflow/project/data/remote/ProjectDtoFun.kt
@@ -2,4 +2,4 @@ package com.task.management.workflow.project.data.remote
 
 import com.task.management.workflow.project.domain.Project
 
-fun ProjectDto.toProject() = Project(name, description, member, leader)
+fun ProjectDto.toProject() = Project(title, description, member, leader)
