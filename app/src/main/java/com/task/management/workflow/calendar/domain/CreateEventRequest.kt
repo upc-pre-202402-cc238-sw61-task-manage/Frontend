595e51a7d7618f6d19eed5b0e77433380073fae2
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2024-11-12 11:08:19 -0500

    fix: changed from init to Long tho match types

--- app/src/main/java/com/task/management/workflow/calendar/domain/CreateEventRequest.kt
+++ app/src/main/java/com/task/management/workflow/calendar/domain/CreateEventRequest.kt
@@ -3,8 +3,8 @@ package com.task.management.workflow.calendar.domain
 import java.time.LocalDate
 
 class CreateEventRequest(
-    val projectId: Int,
-    val userId: Int,
+    val projectId: Long,
+    val userId: Long,
     val title: String,
     val description: String,
     var dueDate: String
